143f6adb578cf5083173e6718bb02972b9030623
Santiago <santiago.dgonzaga@hotmail.com>
2023-12-04 13:45:40 -0300

    initial commit

---
+++ alelo-challenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  alelo-challenge
+//
+//  Created by Santiago del Castillo Gonzaga on 01/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ alelo-challengeUITests/alelo_challengeUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  alelo_challengeUITestsLaunchTests.swift
+//  alelo-challengeUITests
+//
+//  Created by Santiago del Castillo Gonzaga on 01/12/23.
+//
+
+import XCTest
+
+final class alelo_challengeUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
