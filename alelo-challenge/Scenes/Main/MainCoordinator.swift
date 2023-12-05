b906a8a5966cebb106d8b384c94da69eabd1558d
Santiago <santiago.dgonzaga@hotmail.com>
2023-12-05 10:28:15 -0300

    configure viewcode and initial view

---
+++ alelo-challenge/Scenes/Main/MainCoordinator.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+class MainCoordinator: Coordinator {
+    var childCoordinators = [Coordinator]()
+    var navigationController: UINavigationController
+
+    init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+    }
+
+    func start() {
+        let vc = MainViewController()
+        vc.coordinator = self 
+        navigationController.pushViewController(vc, animated: false)
+    }
+}
---
+++ alelo-challenge/Scenes/Main/MainViewController.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    weak var coordinator: MainCoordinator?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .blue
+    }
+
+
+}
+
